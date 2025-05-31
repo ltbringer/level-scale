67b6815a36feed77260e99e334ee884f6411bc24
ltbringer <amresh.venugopal@gmail.com>
2025-05-31 21:39:41 +0100

    add: main func runs http server.

--- src/main.go
+++ src/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"level-scale/dbmanager"
+	"level-scale/logger"
+	"level-scale/models"
+	"level-scale/routes"
+	"level-scale/settings"
+	"net/http"
+)
 
+func main() {
+	logger.Init()
+	settings.Init()
+	dbmanager.Init(settings.DbConfig)
+	err := dbmanager.Db.AutoMigrate(&models.User{}, &models.Shop{}, &models.Product{}, &models.Order{}, &models.OrderItem{}, &models.Return{})
+	if err != nil {
+		logger.Log.Fatalw("Failed to setup models", "err", err)
+	}
+	r := routes.Init()
+	logger.Log.Infow("Starting service", "port", settings.ServicePort)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", settings.ServicePort), r)
+	if err != nil {
+		logger.Log.Fatalw("Failed to start service.", "err", err)
+	}
 }
